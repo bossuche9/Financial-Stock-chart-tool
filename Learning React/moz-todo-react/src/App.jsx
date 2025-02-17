fafe3c4a1fc0a4e10e6c86c0287f74a29b03b311
Uchenna Obi <47361953+bossuche9@users.noreply.github.com>
2025-02-17 22:02:39 +0000

    Added code to App.jsx

--- Learning React/moz-todo-react/src/App.jsx
+++ Learning React/moz-todo-react/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 
 const subject = "React";
-
+/* block comment syntax */
 function App() {
   return (
     <>
       <header>
+      {/* block comment syntax */}
         <h1>Hello, {subject.toUpperCase() + ':)'}!</h1>
         <button type = "button" className="primary">
           Click me!
