debc2ba2ec9948069986d738ab003a1d8b00f423
Uchenna Obi <47361953+bossuche9@users.noreply.github.com>
2025-02-17 22:20:32 +0000

    implemented props functionality

--- Learning React/moz-todo-react/src/App.jsx
+++ Learning React/moz-todo-react/src/App.jsx
@@ -2,12 +2,12 @@ import "./App.css";
 
 const subject = "React";
 /* block comment syntax */
-function App() {
+function App(props) {
   return (
     <>
       <header>
       {/* block comment syntax */}
-        <h1>Hello, {subject.toUpperCase() + ':)'}!</h1>
+        <h1>{props.greeting}, {props.subject}!</h1>
         <button type = "button" className="primary">
           Click me!
         </button>
