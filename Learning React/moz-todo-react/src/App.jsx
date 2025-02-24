5f018429a44122731bd25c9cd775976c877a2d60
bossuche9 <uchennaobi102@gmail.com>
2025-02-23 20:21:12 -0500

    added a lot of functionality

--- Learning React/moz-todo-react/src/App.jsx
+++ Learning React/moz-todo-react/src/App.jsx
@@ -5,9 +5,19 @@ import { useState } from "react";
 import {nanoid} from "nanoid"
 import PropTypes from "prop-types";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
 
  const [tasks, setTasks] = useState(props.tasks);
+ const [filter, setFilter] = useState("All");
+
 
  function addTask(name){
   const newTask = { id: `todo-${nanoid()}`, name, completed: false};
@@ -30,7 +40,19 @@ function App(props) {
     setTasks(remainingTasks);
   }
 
-  const taskList = tasks?.map((task) => (
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) =>{
+      if (id === task.id) { 
+        return {...task, name: newName};
+      } 
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map((task) => (
     <Todo
       id={task.id}
       name={task.name}
@@ -38,10 +60,20 @@ function App(props) {
       key={task.id}
       toggleTaskCompleted = {toggleTaskCompleted}
       deleteTask = {deleteTask}
+      editTask = {editTask}
     />
   ));
 
-  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton 
+    key ={name} 
+    name = {name}
+    isPressed = {name === filter}
+    setFilter = {setFilter}
+     />
+  ));
+
+  const tasksNoun = taskList.length <= 1 ? "task" : "tasks";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
   
   return (
@@ -49,9 +81,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+      {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
@@ -65,7 +95,7 @@ function App(props) {
 }
 
 App.propTypes = {
-    task: PropTypes.arrayOf(
+    tasks: PropTypes.arrayOf(
       PropTypes.shape({
         id: PropTypes.string.isRequired,
         name: PropTypes.string.isRequired,     
