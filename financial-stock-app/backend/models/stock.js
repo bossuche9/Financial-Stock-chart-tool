6c33b66ec1e923d9b82b37963b6b4cfab74e903a
bossuche9 <uchennaobi102@gmail.com>
2025-03-03 13:33:28 -0500

    w

--- financial-stock-app/backend/models/stock.js
+++ financial-stock-app/backend/models/stock.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const StockSchema = new Schema ({
-    symbol: String,
-    currency: String,
-
+    symbol: { type:String, required: true, unique: true},
+    currency: { type:String, required: true},
+    marketCap: { type: Number},
+    percentChange: { type: Number},
 });
 
 module.exports = mongoose.model("Stock", StockSchema);
--- financial-stock-app/backend/models/watchList.js
+++ financial-stock-app/backend/models/watchList.js
@@ -4,7 +4,9 @@ const stock = require("./stock");
 const Schema = mongoose.Schema;
 
 const watchlistSchema = new Schema ({
-    userid: { type: Schema.Types.ObjectId, ref: "User"},
+    userid: { type: Schema.Types.ObjectId, ref: "User", required: true},
     stocks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Stock" }]
 
 })
+
+module.exports = mongoose.model("Watchist",watchlistSchema);
