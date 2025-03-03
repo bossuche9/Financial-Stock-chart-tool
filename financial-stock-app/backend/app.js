cecdaa57081d6ddde222dc32d9b8002e443aeaa9
bossuche9 <uchennaobi102@gmail.com>
2025-03-02 23:39:35 -0500

    node

--- financial-stock-app/backend/app.js
+++ financial-stock-app/backend/app.js
@@ -1,21 +1,29 @@
 require('dotenv').config();
 const express = require("express");
-const moongoose = require("mongoose");
+const mongoose = require("mongoose");
 const cors = require("cors");
-
+const connectDB = require("./config/db");   
 
 const indexRouter = require ("./routes/index");
 
 const app = express();
-
 app.use(express.json());
+app.use(cors())
+
+app.use('/', indexRouter);
+
+connectDB();
+
+const PORT =process.env.PORT || 5000;
+app.listen(PORT, ()=> {
+    console.log(`Server Started at ${PORT}`)
+});
 
  // error handlers
 app.use(function(req, res, next) {
     next(createError(404));
   });
   
-  /
   app.use(function(err, req, res, next) {
    
     res.locals.message = err.message;
