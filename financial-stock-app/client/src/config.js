db850c10e96e5313637e0cf3450343ac28d05316
bossuche9 <uchennaobi102@gmail.com>
2025-05-08 22:29:29 -0400

    Added realtime simulation

--- financial-stock-app/client/src/config.js
+++ financial-stock-app/client/src/config.js
@@ -1,9 +1,9 @@
-const isRunninginCodespaces = () => {
+on const isRunninginCodespaces = () => {
     return window.location.hostname.includes('github.dev');
 }
 
 const API_URL = isRunninginCodespaces()
 ?'https://special-space-meme-4w675g6967rhjpxw-3000.app.github.dev/api'
-: 'http://localhost:3000/api';
+: 'https://financial-stock-chart-tool.onrender.com/api';
 
 export {API_URL};
